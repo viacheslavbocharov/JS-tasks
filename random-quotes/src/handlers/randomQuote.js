b113b9324d33c13b19488bac7a8d75c646ce9dc4
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2025-01-03 17:44:18 +0000

    delete random-quotes

--- random-quotes/src/handlers/randomQuote.js
+++
@@ -1,8 +0,0 @@
-import quotes from '../data/quotes.js';
-import { generateRandomInt } from '../utils/math.js';
-
-function getRandomQuote() {
-  return { ...quotes[generateRandomInt(quotes.length)] };
-}
-
-export { getRandomQuote };
--- random-quotes/src/utils/array.js
+++
@@ -1,9 +0,0 @@
-// Mutates array by removing one object with specific id
-function removeObjectFromArrayById(arr, id) {
-  arr.splice(
-    arr.findIndex((el) => el.id === id),
-    1
-  );
-}
-
-export { removeObjectFromArrayById };

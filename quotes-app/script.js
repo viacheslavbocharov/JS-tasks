dd514bda1490de99e8cdc7f1ace522595c00bbfa
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2025-01-05 13:09:07 +0000

    add favorite card

--- quotes-app/script.js
+++ quotes-app/script.js
@@ -1,59 +1,53 @@
-const quotes = [
-  {
-    quote:
-      'The greatest glory in living lies not in never falling, but in rising every time we fall.',
-    author: 'Nelson Mandela',
-  },
-  {
-    quote: 'The way to get started is to quit talking and begin doing.',
-    author: 'Walt Disney',
-  },
-  {
-    quote: "Don't let yesterday take up too much of today.",
-    author: 'Will Rogers',
-  },
-  {
-    quote:
-      'Success is not final, failure is not fatal: It is the courage to continue that counts.',
-    author: 'Winston Churchill',
-  },
-  {
-    quote: 'You miss 100% of the shots you don’t take.',
-    author: 'Wayne Gretzky',
-  },
-  {
-    quote: 'Whether you think you can or you think you can’t, you’re right.',
-    author: 'Henry Ford',
-  },
-  {
-    quote: 'I have not failed. I’ve just found 10,000 ways that won’t work.',
-    author: 'Thomas Edison',
-  },
-  {
-    quote:
-      'It is our choices, that show what we truly are, far more than our abilities.',
-    author: 'J.K. Rowling',
-  },
-  {
-    quote: 'Life is what happens when you’re busy making other plans.',
-    author: 'John Lennon',
-  },
-  {
-    quote: 'The purpose of our lives is to be happy.',
-    author: 'Dalai Lama',
-  },
-];
+import quotes from './quotes.js';
 
 const quoteElement = document.getElementById('quote');
 const quoteAuthorElement = document.getElementById('quote-author');
 const generateBtn = document.getElementById('generate-btn');
+const toggleFavoriteBtn = document.getElementById('toggle-favorite-btn');
+const favoritesContainer = document.getElementById('favorites-container');
+
+let currentQuoteIndex;
 
 function generateRandomQoute() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
-  const {quote, author} = randomQuote
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  console.log(currentQuoteIndex);
+  const randomQuote = quotes[currentQuoteIndex];
+  const { quote, author } = randomQuote;
   quoteElement.textContent = quote;
   quoteAuthorElement.textContent = author;
+  toggleFavoriteBtn.textContent = randomQuote.isFavorite
+    ? 'Remove from Favorites'
+    : 'Add to Favorites';
+
+  toggleFavoriteBtn.style.display = 'inline-block';
+}
+
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  toggleFavoriteBtn.textContent = currentQuote.isFavorite
+    ? 'Remove from Favorites'
+    : 'Add to Favorites';
+
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement('div');
+    favoriteCard.classList.add('favorite-card');
+    favoriteCard.innerHTML = `
+    <p>${currentQuote.quote}</p>
+    <p class="favorite-card-author">${currentQuote.author}</p>
+    `;
+    favoritesContainer.appendChild(favoriteCard);
+  } else {
+    const favoriteCards = document.querySelectorAll('.favorite-card');
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(currentQuote.quote)) {
+        card.remove();
+      }
+    });
+  }
 }
 
 generateBtn.addEventListener('click', generateRandomQoute);
+toggleFavoriteBtn.addEventListener('click', toggleFavorite);
+
+// generateRandomQoute();

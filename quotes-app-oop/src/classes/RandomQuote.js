d6221b989edf488f548bd37332bcf439750b9fcb
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2025-01-14 11:17:19 +0000

    get random quotes via API

--- quotes-app-oop/src/classes/RandomQuote.js
+++ quotes-app-oop/src/classes/RandomQuote.js
@@ -10,6 +10,35 @@ class RandomQuote {
     //localStorage.setItem('currentQuote', JSON.stringify(randomQuote));
     return randomQuote;
   }
+
+  static getRandomQuoteViaAPI() {
+    const url = 'https://quoteslate.vercel.app/api/quotes/random';
+    const options = { headers: { 'Content-Type': 'application/json' } };
+
+    return fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        const { id, quote: text, author } = data;
+        return new Quote(id, text, author);
+      })
+      .catch((error) => console.error(error));
+  }
+
+  // static async getRandomQuoteViaAPI() {
+  //   try {
+  //     const response = await fetch(
+  //       'https://quoteslate.vercel.app/api/quotes/random'
+  //     );
+  //     const data = await response.json();
+  //     const { id, quote: text, author } = data;
+  //     const randomQuote = new Quote(id, text, author);
+  //     console.log(randomQuote);
+  //     return randomQuote;
+  //   } catch (error) {
+  //     console.error('Error fetching random quote:', error);
+  //     return null;
+  //   }
+  // }
 }
 
 export default RandomQuote;
--- quotes-app-oop/src/classes/RandomQuotesApp.js
+++ quotes-app-oop/src/classes/RandomQuotesApp.js
@@ -1,8 +1,10 @@
 import RandomQuote from './RandomQuote.js';
+import Quote from './Quote.js';
 
 class RandomQuotesApp {
   constructor() {
     this.randomQuoteBtn = document.getElementById('random-quote-btn');
+    this.randomQuoteApiBtn = document.getElementById('random-quote-api-btn');
     this.quoteTextElement = document.getElementById('quote-text');
     this.quoteAuthorElement = document.getElementById('quote-author');
     this.currentQuote = null;
@@ -10,19 +12,32 @@ class RandomQuotesApp {
   }
 
   displayCurrentQuote() {
-    const { author } = this.currentQuote;
     this.quoteTextElement.textContent = this.currentQuote.formatText();
     this.quoteAuthorElement.textContent = this.currentQuote.formatAuthor();
   }
 
+  changeCurrentQuote(newQuote) {
+    if (newQuote instanceof Quote) {
+      this.currentQuote = newQuote;
+      this.displayCurrentQuote();
+    }
+  }
+
   getRandomQuote() {
-    const randomQuote = RandomQuote.getRandomQuote();
-    this.currentQuote = randomQuote;
-    this.displayCurrentQuote();
+    this.changeCurrentQuote(RandomQuote.getRandomQuote());
+  }
+
+  getRandomQuoteViaAPI() {
+    RandomQuote.getRandomQuoteViaAPI().then((quote) =>
+      this.changeCurrentQuote(quote)
+    );
   }
 
   init() {
     this.randomQuoteBtn.addEventListener('click', () => this.getRandomQuote());
+    this.randomQuoteApiBtn.addEventListener('click', () =>
+      this.getRandomQuoteViaAPI()
+    );
   }
 }
 
